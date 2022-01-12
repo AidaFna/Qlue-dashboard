d3c0169ec0129b3c6ceddb51b47f64e0e9e150a9
AidaFna <93112554+AidaFna@users.noreply.github.com>
2022-01-13 00:29:19 +0700

    add Data Page

---
+++ src/views/data/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Col, Row } from "react-bootstrap";
+import Navbar from "../components/navbar";
+import Navigation from "../components/navigation";
+import Tables from "./table";
+import "./data.css";
+
+const Data = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <Container>
+        <Navigation />
+        <h1 className="mt-5">Data</h1>
+        <Row className="mt-5">
+          <Col md={12} sm={12}>
+            <Tables />
+          </Col>
+          <Col md={12} sm={12}></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Data;

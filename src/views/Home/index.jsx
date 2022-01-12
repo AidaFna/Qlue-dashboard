5e280f96d17405689f6dbdde3cf1bf5ea3e9cb14
AidaFna <93112554+AidaFna@users.noreply.github.com>
2022-01-12 19:43:18 +0700

    fix error

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
-import { useReducer, useSelector } from "react-redux";
-import { Container, Spinner } from "react-bootstrap";
+// import { useState, useEffect } from "react";
+// import { useReducer, useSelector } from "react-redux";
+import {
+  Container,
+  // Spinner
+} from "react-bootstrap";
 import "./home.css";
-import axios from "axios";
-import allStore from "../../store/actions";
-import { useDispatch } from "react-redux";
+// import axios from "axios";
+// import allStore from "../../store/actions";
+// import { useDispatch } from "react-redux";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
+  // const dispatch = useDispatch();
+  // const [loading, setLoading] = useState(false);
 
-  if (loading) {
-    return <Spinner className="spinner" animation="border" variant="success" />;
-  }
+  // if (loading) {
+  //   return <Spinner className="spinner" animation="border" variant="success" />;
+  // }
   return (
     <div className="todo">
       <Container></Container>
